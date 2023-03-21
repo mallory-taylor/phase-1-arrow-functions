c5894bca8be2503453521ed7e95af5321f1cfb10
Mallory Taylor <malloryataylor13@gmail.com>
2023-03-21 17:21:56 -0400

    Function expression and arrow functions

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function(num1, num2) {
+return num1 / num2;
+}
+divide(2000, 100);
+
+const square = parameter1 => parameter1 ** 2;
+
+const add = (number1, number2) => number1 + number2;
